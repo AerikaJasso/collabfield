5f179b5db1e87ad61130dd79668b24340502ce4c
Ærika <aerikajasso@gmail.com>
2019-11-12 21:31:39 -0800

    Add specs to NavigationHelper's collapsible_links_partial_path method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,15 +7,6 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-
-require 'capybara/poltergeist'
-require 'factory_girl_rails'
-require 'capybara/rspec'
-
-config.include Devise::Test::IntegrationHelpers, type: :feature
-config.include FactoryGirl::Syntax::Methods
-Capybara.javascript_driver = :poltergeist
-Capybara.server = :puma
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -89,4 +80,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  require 'capybara/poltergeist'
+  require 'factory_girl_rails'
+  require 'capybara/rspec'
+
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include FactoryGirl::Syntax::Methods
+  Capybara.default_max_wait_time = 10
+  Capybara.javascript_driver = :poltergeist
+  Capybara.server = :puma
 end
